7b34f7f5a3058ccf6e180f72d66b7f05cedc4b28
Minwoo Na <natorque5552@naver.com>
2022-05-09 08:44:50 +0900

    --

--- src/composables/auth.js
+++ src/composables/auth.js
@@ -5,24 +5,24 @@
  */
 
 
-import { useStore } from 'vuex';
+ import { useStore } from 'vuex';
 
-export const useAuth = () => {
-    const store = useStore();
-    
-    const userInfo = {
-        id: store.state.auth.id,
-        token: store.state.auth.token,
-    }
-
-    const logout = () => { // 로그아웃 기능
-        store.dispatch('auth/logout', {
-            
-        });
-    }
-
-    return {
-        userInfo,
-        logout,
-    }
-}
+ export const useAuth = () => {
+     const store = useStore();
+     
+     const userInfo = {
+         id: store.state.auth.id,
+         token: store.state.auth.token,
+     }
+ 
+     const logout = () => { // 로그아웃 기능
+         store.dispatch('auth/logout', {
+             
+         });
+     }
+ 
+     return {
+         userInfo,
+         logout,
+     }
+ }
--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -5,35 +5,35 @@
  */
 
 
-import router from '@/router' 
+ import router from '@/router' 
 
-export default { 
-    state: { // 유저 정보 더 추가하기!
-        id: '',
-        token: '' 
-    }, 
-
-    mutations: { 
-        ADD_USER_INFO(state, payload) { 
-            state.id = payload.id 
-            state.token = payload.token 
-        }, 
-        REMOVE_USER_INFO(state) { 
-            state.id = '';
-            state.token = '';
-        } 
-    },
-
-    actions: {
-        logout({ commit }) { // 로그아웃
-            commit('REMOVE_USER_INFO');
-            router.push({
-                name: 'Login'
-            });
-        }
-    },
-
-    getters: {
-
-    }
-} 
+ export default { 
+     state: { // 유저 정보 더 추가하기!
+         id: '',
+         token: '' 
+     }, 
+ 
+     mutations: { 
+         ADD_USER_INFO(state, payload) { 
+             state.id = payload.id 
+             state.token = payload.token 
+         }, 
+         REMOVE_USER_INFO(state) { 
+             state.id = '';
+             state.token = '';
+         } 
+     },
+ 
+     actions: {
+         logout({ commit }) { // 로그아웃
+             commit('REMOVE_USER_INFO');
+             router.push({
+                 name: 'Login'
+             });
+         }
+     },
+ 
+     getters: {
+ 
+     }
+ } 
